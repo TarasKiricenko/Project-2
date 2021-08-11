2c8f52161d319032f9ac342a3309dc32b9c716bc
TarasKiricenko <81250034+TarasKiricenko@users.noreply.github.com>
2021-08-11 18:02:23 +0300

    Update App.js
    Unused code deleted.

--- src/App.js
+++ src/App.js
@@ -38,12 +38,3 @@ function App() {
 }
 
 export default App
-
-// drop down by percentage
-
-
-// when user click on any beer ( id) we render them to another page of that beer with more info, description ingredient
-// ( also add ingredients, hops, style, year of first production, food pairing etc.) 
-//! add anther link (button type, or any JSX) displaying brewer tips for the beer
-//! we need to get another button to go back to homepage or to see all the beers.
-
